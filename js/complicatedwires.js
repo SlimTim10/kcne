753a978fe90cc1e7770289d3fd503072574240ff
SlimTim10 <slimtim10@gmail.com>
2015-11-10 11:22:49 -0500

    Finish complicated wires solve. Need to refactor inline CSS

--- js/complicatedwires.js
+++ js/complicatedwires.js
@@ -17,6 +17,9 @@ kcneApp.controller("ComplicatedWiresCtrl", function($scope) {
 				solution: ""
 			});
 		}
+		$scope.serialNumberEven = "";
+		$scope.parallelPort = "";
+		$scope.batteries = "";
 	};
 
 	$scope.toggleLED = function(n) {
@@ -31,8 +34,118 @@ kcneApp.controller("ComplicatedWiresCtrl", function($scope) {
 		$scope.combinations[n]["star"] = !$scope.combinations[n]["star"];
 	};
 
+	$scope.selectSerialNumberEven = function(b) {
+		$scope.serialNumberEven = b;
+	};
+
+	$scope.selectParallelPort = function(b) {
+		$scope.parallelPort = b;
+	};
+
+	$scope.selectBatteries = function(n) {
+		$scope.batteries = n;
+	};
+
 	$scope.solve = function() {
-		;
+		for (var i = 0; i < $scope.combinations.length; i++) {
+			$scope.combinations[i]["solution"] = cutOrNot($scope.combinations[i]);
+		}
+	};
+
+	var cutOrNot = function(combination) {
+		var red = combination["wire"]["red"];
+		var blue = combination["wire"]["blue"];
+		var white = combination["wire"]["white"];
+		var star = combination["star"];
+		var LED = combination["LED"];
+		var cut = false;
+
+		// No wire
+		if (!red && !blue && !white) {
+			return "";
+		}
+
+		if ($scope.serialNumberEven === "") {
+			return "Check the serial number!";
+		}
+		if ($scope.parallelPort === "") {
+			return "Check for a parallel port!";
+		}
+		if ($scope.batteries === "") {
+			return "Check the number of batteries!";
+		}
+
+		if (
+			(!red &&
+			 !blue &&
+			 !star &&
+			 !LED) ||
+				
+			(red &&
+			 !blue &&
+			 star &&
+			 !LED) ||
+				
+			(!red &&
+			 !blue &&
+			 star &&
+			 !LED) ||
+				
+			(red &&
+			 !blue &&
+			 !star &&
+			 !LED &&
+			 $scope.serialNumberEven) ||
+
+			(!red &&
+			 blue &&
+			 !star &&
+			 !LED &&
+			 $scope.serialNumberEven) ||
+
+			(red &&
+			 blue &&
+			 !star &&
+			 !LED &&
+			 $scope.serialNumberEven) ||
+
+			(!red &&
+			 blue &&
+			 !star &&
+			 LED &&
+			 $scope.parallelPort) ||
+
+			(red &&
+			 blue &&
+			 star &&
+			 !LED &&
+			 $scope.parallelPort) ||
+
+			(!red &&
+			 blue &&
+			 star &&
+			 LED &&
+			 $scope.parallelPort) ||
+
+			(red &&
+			 !blue &&
+			 LED &&
+			 $scope.batteries >= 2) ||
+
+			(!red &&
+			 !blue &&
+			 star &&
+			 LED &&
+			 $scope.batteries >= 2)
+		) {
+			cut = true;
+		}
+
+		if (cut) {
+			return "Cut!";
+		} else {
+			return "Do not cut.";
+		}
 	};
 
 	init();
